a395bc8540fb3ad85711e20f2b5d92c60ba0728d
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-09-14 13:27:18 +0100

    Completed 3rd day backend structure

---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,7 @@
+export function convertHourStringToMinutes(hourString: string) {
+  const [hours, minutes] = hourString.split(":").map(Number);
+
+  const minutesTotal = (hours * 60) + minutes;
+
+  return minutesTotal;
+}
